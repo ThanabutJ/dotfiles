ca067fa001ca94e113e9ed0f45c9e71be5fed411
Zoom arco <thanabut.j@gmail.com>
2021-04-15 02:02:27 +0700

    add more shortcut to dmenu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -314,6 +314,13 @@ globalkeys = my_table.join(
 	end,
     {description = "show dmenu", group = "hotkeys"}),
 
+    awful.key({ altkey }, "space",
+    function ()
+        awful.spawn(string.format("dmenu_run -i -nb '#191919' -nf '#fea63c' -sb '#fea63c' -sf '#191919' -fn NotoMonoRegular:bold:pixelsize=14",
+        beautiful.bg_normal, beautiful.fg_normal, beautiful.bg_focus, beautiful.fg_focus))
+	end,
+    {description = "show dmenu", group = "hotkeys"}),
+
     -- Function keys
     awful.key({ }, "F12", function () awful.util.spawn( "xfce4-terminal --drop-down" ) end,
         {description = "dropdown terminal" , group = "function keys"}),
