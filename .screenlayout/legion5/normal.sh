43f8b381f18df82d4c70bcd61868d28219d909aa
Zoom arco <thanabut.j@gmail.com>
2021-07-08 20:03:25 +0700

    add screenlayout for arandr

---
+++ .screenlayout/legion5/normal.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 1971x1431 --rotate normal --output DP-2 --off --output eDP-1-1 --primary --mode 1920x1080 --pos 51x1431 --rotate normal
