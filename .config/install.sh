32c69e19fb578a7c24238244d5f5708184f0ba13
ThanabutJ <thanabut.j@gmail.com>
2021-07-22 19:03:50 +0700

    remove hardcode array

--- .config/install.sh
+++ .config/install.sh
@@ -3,8 +3,6 @@
 awesome_dir=~/.config/awesome
 my_awesome_dir=$DOTFILEPATH/.config/awesome
 
-awesome_files=("rc.lua" "autostart.sh" "picom.conf" "system-overview")
-
 for f in `ls -1 awesome`; do
     echo remove $f
     rm -rf $awesome_dir/$f
